04acc090accc26d88307c515fde7af3d39c73751
ishaangrewal <ishaangrewal2001@gmail.com>
2023-11-25 01:51:32 -0600

    find best step paralellized

--- decisiontrees.cpp
+++ decisiontrees.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <pthread.h>
+#include <atomic>
+
 
 using namespace std;
 
@@ -15,6 +18,15 @@ struct DecisionTreeNode {
 struct Point {
     vector<float> features;
     int label;
+};
+
+struct ThreadArgs {
+    int* threadIndex;
+    int* numThreads;
+    const vector<Point>* data;
+    vector<float>* bestGini;
+    vector<int>* bestFeatureIndex;
+    vector<float>* bestThreshold;
 };
 
 float calculateGini(const vector<Point>& data) {
@@ -62,8 +74,48 @@ pair <int, float> findBestSplit(const vector<Point>& data) {
     return make_pair(bestFeatureIndex, bestThreshold);
 }
 
+void* findBestSplitParallelThread(void* args) {
+    ThreadArgs* threadArgs = static_cast<ThreadArgs*>(args);
+    int threadIndex = *(threadArgs->threadIndex);
+    int numThreads = *(threadArgs->numThreads);
+    for (int featureIndex = threadIndex; featureIndex < (*(threadArgs->data))[0].features.size(); featureIndex+= numThreads) {
+        for (int i = 0; i < threadArgs->data->size(); i++) {
+            float threshold = (*(threadArgs->data))[i].features[featureIndex];
+            pair<vector<Point>, vector<Point> > split = splitData((*(threadArgs->data)), featureIndex, threshold);
+            float gini = calculateGini(split.first) * (split.first.size() / (threadArgs->data->size() + 0.0)) + calculateGini(split.second) * (split.second.size() / (threadArgs->data->size() + 0.0));
+            if (gini < (*(threadArgs->bestGini))[threadIndex]) {
+                (*(threadArgs->bestGini))[threadIndex] = gini;
+                (*(threadArgs->bestFeatureIndex))[threadIndex] = featureIndex;
+                (*(threadArgs->bestThreshold))[threadIndex] = threshold;
+            }
+        }
+    }
+    return NULL;
+}
 
-
+pair <int, float> findBestSplitParallel(const vector<Point>& data, const int numThreads) {
+    float bestGini = 1.0;
+    int bestFeatureIndex = -1;
+    float bestThreshold = 0.0;
+    vector<float> bestGinis(numThreads, 1.0);
+    vector<int> bestFeatureIndices(numThreads, -1);
+    vector<float> bestThresholds(numThreads, 0.0);
+    pthread_t threads[numThreads];
+    for (int threadIndex = 0; threadIndex < numThreads; threadIndex++) {
+        pthread_create(&threads[threadIndex], NULL, findBestSplitParallelThread, new ThreadArgs{new int(threadIndex), new int(numThreads), &data, &bestGinis, &bestFeatureIndices, &bestThresholds});
+    }
+    for (int threadIndex = 0; threadIndex < numThreads; threadIndex++) {
+        pthread_join(threads[threadIndex], NULL);
+    }
+    for (int threadIndex = 0; threadIndex < numThreads; threadIndex++) {
+        if (bestGinis[threadIndex] < bestGini) {
+            bestGini = bestGinis[threadIndex];
+            bestFeatureIndex = bestFeatureIndices[threadIndex];
+            bestThreshold = bestThresholds[threadIndex];
+        }
+    }
+    return make_pair(bestFeatureIndex, bestThreshold);
+}
 
 DecisionTreeNode* sequentialBuildDecisionTree(const vector<Point>& data, const int depthLeft) {
     if (data.size() == 0) {
@@ -79,7 +131,8 @@ DecisionTreeNode* sequentialBuildDecisionTree(const vector<Point>& data, const i
     if (classCounts[1] == data.size()) {
         return new DecisionTreeNode{-1, 0.0, 1, NULL, NULL};
     }
-    pair<int, float> bestSplit = findBestSplit(data);
+    //pair<int, float> bestSplit = findBestSplit(data);
+    pair<int, float> bestSplit = findBestSplitParallel(data, 2);
     if (depthLeft == 0 || bestSplit.first == -1) {
         int majority = classCounts[0] > classCounts[1] ? 0 : 1;
         return new DecisionTreeNode{-1, 0.0, majority, NULL, NULL};
@@ -111,25 +164,21 @@ int main() {
     data.push_back({{6.0, 3.0}, 1});
     data.push_back({{7.0, 1.0}, 1});
     data.push_back({{8.0, 2.0}, 1});
-    DecisionTreeNode* root = sequentialBuildDecisionTree(data, 1);
-    // cout << (predict(root, {{1.0, 2.0}, 0}) == 0) << endl;
-    // cout << (predict(root, {{2.0, 1.0}, 0}) == 0) << endl;
-    // cout << (predict(root, {{2.0, 3.0}, 0}) == 0) << endl;
-    // cout << (predict(root, {{3.0, 2.0}, 0}) == 0) << endl;
-    // cout << (predict(root, {{5.0, 3.0}, 1}) == 1) << endl;
-    // cout << (predict(root, {{6.0, 3.0}, 1}) == 1) << endl;
-    // cout << (predict(root, {{7.0, 1.0}, 1}) == 1) << endl;
-    // cout << (predict(root, {{8.0, 2.0}, 1}) == 1) << endl;
+    DecisionTreeNode* root = sequentialBuildDecisionTree(data, 4);
+    cout << (predict(root, {{1.0, 2.0}, 0}) == 0) << endl;
+    cout << (predict(root, {{2.0, 1.0}, 0}) == 0) << endl;
+    cout << (predict(root, {{2.0, 3.0}, 0}) == 0) << endl;
+    cout << (predict(root, {{3.0, 2.0}, 0}) == 0) << endl;
+    cout << (predict(root, {{5.0, 3.0}, 1}) == 1) << endl;
+    cout << (predict(root, {{6.0, 3.0}, 1}) == 1) << endl;
+    cout << (predict(root, {{7.0, 1.0}, 1}) == 1) << endl;
+    cout << (predict(root, {{8.0, 2.0}, 1}) == 1) << endl;
 
 
     // cout << root->featureIndex << endl;
     // cout << root->threshold << endl;
     // cout << root->label << endl << endl;
-    // cout << root->left->featureIndex << endl;
-    // cout << root->left->threshold << endl;
-    // cout << root->left->label << endl << endl;
-    // cout << root->right->featureIndex << endl;
-    // cout << root->right->threshold << endl;
-    // cout << root->right->label << endl << endl;
+    // cout << root->left << endl;
+    // cout << root->right << endl;
     return 0;
 }
